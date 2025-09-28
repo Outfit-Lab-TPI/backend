f6506bc9edf60eee64b85291f9ebb6df80a6b03a
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2025-09-28 15:36:35 -0300

    primer commit

---
+++ src/test/java/com/outfitlab/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outfitlab.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
