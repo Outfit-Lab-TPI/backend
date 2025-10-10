2dc8448244479ada0714810576952cb3ea0db134
FeliWeigel <fweigeldev@gmail.com>
2025-10-10 19:00:52 -0300

    S3 Storage final implementation

--- src/test/java/com/outfitlab/project/infrastructure/TrippoServiceTest.java
+++ src/test/java/com/outfitlab/project/infrastructure/TrippoServiceTest.java
@@ -16,7 +16,6 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
