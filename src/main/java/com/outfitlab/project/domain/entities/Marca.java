36434fd4ca906c6661bf7c86bc2469b2fb81178c
Tomas Nania <tnania2022@gmail.com>
2025-10-13 01:48:57 -0300

    ft/marcas y /marcas/codigoMarca hecho

---
+++ src/main/java/com/outfitlab/project/domain/entities/Marca.java
@@ -0,0 +1,45 @@
+package com.outfitlab.project.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Marca {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private String codigoMarca;
+    @Column(nullable = false)
+    private String logoUrl;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
+
+}
