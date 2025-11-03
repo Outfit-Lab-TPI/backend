5d7b512bf83ff8e8933843ace7884b721b127d60
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2025-11-03 19:51:13 -0300

    ft/refactor - refactor paginado de /marcas

--- src/main/java/com/outfitlab/project/domain/interfaces/repositories/BrandRepository.java
+++ src/main/java/com/outfitlab/project/domain/interfaces/repositories/BrandRepository.java
@@ -1,9 +1,11 @@
 package com.outfitlab.project.domain.interfaces.repositories;
 
 import com.outfitlab.project.domain.model.BrandModel;
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 public interface BrandRepository {
     BrandModel findByBrandCode(String brandCode);// hay que hacer un DTO que no me deje traer el id
-    List<BrandModel> getAllBrands(int page);
+    Page<BrandModel> getAllBrands(int page);
 }
--- src/main/java/com/outfitlab/project/presentation/BrandController.java
+++ src/main/java/com/outfitlab/project/presentation/BrandController.java
@@ -1,11 +1,16 @@
 package com.outfitlab.project.presentation;
 
 import com.outfitlab.project.domain.exceptions.PageLessThanZeroException;
+import com.outfitlab.project.domain.model.dto.BrandDTO;
 import com.outfitlab.project.domain.useCases.marca.GetAllBrands;
 import com.outfitlab.project.domain.exceptions.BrandsNotFoundException;
 import com.outfitlab.project.domain.useCases.marca.GetBrandAndGarmentsByBrandCode;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -22,12 +27,23 @@ public class BrandController {
     @GetMapping("/marcas")
     public ResponseEntity<?> getMarcas(@RequestParam(defaultValue = "0") int page) {
         try {
-            return ResponseEntity.ok(this.getAllMarcas.execute(page));
+            return ResponseEntity.ok(buildResponse(this.getAllMarcas.execute(page)));
         } catch (BrandsNotFoundException | PageLessThanZeroException e) {
             return ResponseEntity
                     .status(404)
                     .body(e.getMessage());
         }
+    }
+
+    private static Map<String, Object> buildResponse(Page<BrandDTO> response) {
+        Map<String, Object> pageResponse = new HashMap<>();
+        pageResponse.put("content", response.getContent());
+        pageResponse.put("page", response.getNumber());
+        pageResponse.put("size", response.getSize());
+        pageResponse.put("totalElements", response.getTotalElements());
+        pageResponse.put("totalPages", response.getTotalPages());
+        pageResponse.put("last", response.isLast());
+        return pageResponse;
     }
 
     @GetMapping("/marcas/{brandCode}")
