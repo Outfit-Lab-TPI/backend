b5df7deabbec087768db64e0767f4322d6a39ad8
rioslucas1 <rioslucasariel01@gmail.com>
2025-11-24 01:43:47 -0300

    debug: Arreglo de consulta de sql para limites de planes

--- src/main/java/com/outfitlab/project/infrastructure/repositories/interfaces/UserSubscriptionJpaRepository.java
+++ src/main/java/com/outfitlab/project/infrastructure/repositories/interfaces/UserSubscriptionJpaRepository.java
@@ -9,23 +9,23 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface UserSubscriptionJpaRepository extends JpaRepository<UserSubscriptionEntity, Long> {
-    
+
     @Query("SELECT us FROM UserSubscriptionEntity us JOIN FETCH us.user u JOIN FETCH us.subscription WHERE u.email = :email")
     Optional<UserSubscriptionEntity> findByUserEmail(@Param("email") String email);
-    
+
     @Modifying
-    @Query("UPDATE UserSubscriptionEntity u SET u.combinationsUsed = u.combinationsUsed + 1 WHERE u.user.email = :email")
-    void incrementCombinations(@Param("email") String email);
-    
+    @Query("UPDATE UserSubscriptionEntity u SET u.combinationsUsed = u.combinationsUsed + 1 WHERE u.user.id = :userId")
+    void incrementCombinationsByUserId(@Param("userId") Long userId);
+
     @Modifying
-    @Query("UPDATE UserSubscriptionEntity u SET u.favoritesCount = u.favoritesCount + 1 WHERE u.user.email = :email")
-    void incrementFavorites(@Param("email") String email);
-    
+    @Query("UPDATE UserSubscriptionEntity u SET u.favoritesCount = u.favoritesCount + 1 WHERE u.user.id = :userId")
+    void incrementFavoritesByUserId(@Param("userId") Long userId);
+
     @Modifying
-    @Query("UPDATE UserSubscriptionEntity u SET u.favoritesCount = u.favoritesCount - 1 WHERE u.user.email = :email AND u.favoritesCount > 0")
-    void decrementFavorites(@Param("email") String email);
-    
+    @Query("UPDATE UserSubscriptionEntity u SET u.favoritesCount = u.favoritesCount - 1 WHERE u.user.id = :userId AND u.favoritesCount > 0")
+    void decrementFavoritesByUserId(@Param("userId") Long userId);
+
     @Modifying
-    @Query("UPDATE UserSubscriptionEntity u SET u.modelsGenerated = u.modelsGenerated + 1 WHERE u.user.email = :email")
-    void incrementModels(@Param("email") String email);
+    @Query("UPDATE UserSubscriptionEntity u SET u.modelsGenerated = u.modelsGenerated + 1 WHERE u.user.id = :userId")
+    void incrementModelsByUserId(@Param("userId") Long userId);
 }
